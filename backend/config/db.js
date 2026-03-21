286909f89aa49ecfaccff2e77b229f0a9404b718
msdaashish <msdaashish@gmail.com>
2026-03-21 20:01:01 +0530

    chore: fix code formatting & update lint-staged in package.json script file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        // uncomment this before pushing changes
-        logging: false // To prevent unnecessary logging of sql queries
-    }
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    // uncomment this before pushing changes
+    logging: false, // To prevent unnecessary logging of sql queries
+  }
 );
--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,18 +1,18 @@
 import { DataTypes } from 'sequelize';
-import {sequelize} from '../config/db.js'
+import { sequelize } from '../config/db.js';
 
 export const Expense = sequelize.define('Expense', {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    amount: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: "Miscellaneous"
-    }
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'Miscellaneous',
+  },
 });
